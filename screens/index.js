ca547191e270b99b123eaed60cf6d19a3d98338c
Jose Elias Barraza <elj44@hotmail.com>
2019-01-20 14:33:37 -0500

    first commit

---
+++ screens/index.js
@@ -0,0 +1,11 @@
+import Login from '../screens/Login';
+import Home from '../screens/Home';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+export {
+  Login,
+  Home,
+  LinksScreen,
+  SettingsScreen
+}
